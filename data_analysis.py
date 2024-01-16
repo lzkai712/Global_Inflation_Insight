1aa33a64b1ee5fc1bea8df0b6cc21a5c44672607
zhongkailin <zhongkai670.revature.net>
2024-01-15 23:16:11 -0500

    First commit- version not working yet

---
+++ data_analysis.py
@@ -0,0 +1,36 @@
+from database import connect
+
+def search_country_data():
+    # Implement logic to search and display inflation data for a specific country
+    pass
+
+def identify_country_inflation():
+    # Implement logic to identify and display a country's highest or lowest inflation rate
+    pass
+
+def identify_global_highest_inflation():
+    # Implement logic to identify and display the country with the highest inflation at a specific year
+    pass
+
+def compare_countries():
+    # Implement logic to compare and display inflation rates between different countries
+    pass
+def highest_lowest_inflation(country):
+    # Implement logic to find highest and lowest inflation for a country
+    pass
+
+def global_highest_inflation(year):
+    # Implement logic to find the country with the highest inflation for a specific year
+    pass
+
+def compare_countries(countries, year):
+    # Implement logic to compare inflation rates between countries for a specific year
+    pass
+
+def manage_add_data():
+    # Implement logic to add, update, or delete inflation data
+    pass
+def manage_update_data():
+    pass
+def manage_delete_data():
+    pass
---
+++ user_management.py
@@ -0,0 +1,68 @@
+import getpass
+from database import connect
+
+MANAGER_ACCOUNT = {
+    "username": "zhongkai670@revature.net",
+    "password": "123456789",
+    "name" : "Manager"
+}
+
+def is_manager(username):
+    return username == MANAGER_ACCOUNT["username"]
+
+def register():
+    """User registration process"""
+    conn = connect()
+    cur = conn.cursor()
+
+    while True:
+        f_name = input("Enter your first name: ").title()
+        l_name = input("Enter your Last name: ").title()
+        dob = input("Enter your birthday in mm/dd/yyyy: ")
+        location = input("Enter city of your location(Optional): ")
+        username = input("Enter your email to create account: ").lower()
+        password = getpass.getpass("Enter your password: ")
+        #Check if user already existed
+        cur.execute('SELECT * FROM users WHERE username = ?', (username,))
+        existing_user = cur.fetchone()
+
+        if existing_user:
+            print("Email already exists. Please double check and try again: ")
+        else:
+            cur.execute('''
+                        INSERT INTO users (FistName, LastName, DateOfBirth, Location, Username, Password) 
+                        VALUES(?, ?, ?, ?, ?, ?) 
+                        ''',(f_name, l_name, dob, location,username, password))
+            conn.commit()
+
+            print("Registration successful!")
+            break
+        conn.close()
+
+def login():
+    """Function for check logging in procedure"""
+    conn = connect()
+    cur = conn.cursor()
+
+    while True:
+        username = input("Enter your email: ").lower()
+        password = getpass.getpass("Enter your password: ")
+
+        if username == MANAGER_ACCOUNT["username"] and password == MANAGER_ACCOUNT("password"):
+            conn.close()
+            return MANAGER_ACCOUNT
+        else:
+            cur.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+            user = cur.fetchone()
+            if user:
+                print("Login successful!")
+                conn.close()
+                return user
+            else:
+                print("Invalid username or password. Please try again.")
+        
+        try_again = input("Do you want to try again? (y/n): ").lower()
+        if try_again == 'n':
+            break
+    conn.close()
+    
